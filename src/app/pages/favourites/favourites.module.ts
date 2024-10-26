2429d714898048810100bb1b4d13a67540fa7ea1
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-10-26 17:39:20 +0200

    Favourites,login and register pages

--- src/app/pages/favourites/favourites.module.ts
+++ src/app/pages/favourites/favourites.module.ts
@@ -4,14 +4,8 @@ import { CommonModule } from '@angular/common';
 import { FavouritesRoutingModule } from './favourites-routing.module';
 import { FavouritesComponent } from './favourites.component';
 
-
 @NgModule({
-  declarations: [
-    FavouritesComponent
-  ],
-  imports: [
-    CommonModule,
-    FavouritesRoutingModule
-  ]
+  declarations: [FavouritesComponent],
+  imports: [CommonModule, FavouritesRoutingModule],
 })
-export class FavouritesModule { }
+export class FavouritesModule {}
